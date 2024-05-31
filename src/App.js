fb831556a0b761eb0159b64883da28d4fcc3e377
Saravanan <51911893+saravanan2432@users.noreply.github.com>
2024-05-31 17:09:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState,useEffect } from "react";
+import axios from "axios";
+import './style.css'
+function WeatherReport(){
+  const[weather,setWeather]=useState(null)
+  const[city,setCity]=useState(null)
+  
+  useEffect(()=>{
+    if(city!==null){
+      
+const baseURL="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8a1cf5c6d66c98e6d56d61a092f0c73f"
+      axios.get(baseURL).then((response)=>{
+        console.log(response.data)
+        setWeather(response.data)   
+      }).catch((error)=>{
+        setWeather(error.response.data)
+      })
+    }
+  })
+    const display=()=>{
+    setCity(document.getElementById("cityName").value)
+    }
+  return(
+    <div className="Display">
+        <h1><font color="red">W</font>eather <font color="red">R</font>eport <font color="red">U</font>sing <font color="red">A</font>xios</h1>
+    <input type="text" id="cityName" placeholder="Search here....."></input><br></br><br></br>
+    <input type="button" onClick={display} value="Weather Report" id="Btn"></input>
+    {weather!==null && 
+    <>
+    {weather.cod==="404" && "City Not Found"}  
+    {weather.cod===200 && 
+    <>
+    <br></br><br></br><br></br><br></br><br></br><br></br>
+    <table border={2}>
+      <tr>City Name<td>{weather.name}</td></tr>
+      <tr>Weather Report<td>{weather.weather[0].main}</td></tr>
+      <tr>Temperature<td>{weather.main.temp}<sup><small><font color="red">o</font></small></sup>C</td></tr>
+      <tr>Status code<td>{weather.cod}</td></tr>
+    </table>
+</>
 }
-
-export default App;
+</>
+}
+    </div>
+  )
+}
+export default WeatherReport
